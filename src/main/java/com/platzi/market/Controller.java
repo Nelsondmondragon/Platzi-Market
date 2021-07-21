6ba0af6bc10c4758a4d18dec40a1846e70503a1a
Nelson Cortes <nelsondmondragon@gmail.com>
2021-07-21 08:22:18 -0500

    Spring Initializr, Hello world

---
+++ src/main/java/com/platzi/market/Controller.java
@@ -0,0 +1,15 @@
+package com.platzi.market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greet")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String greet() {
+        return "Hello word";
+    }
+}
